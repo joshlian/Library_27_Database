8fa0e15f93fb01c1e9088b7a5f7d417e66f8239a
joshlian <123327816+joshlian@users.noreply.github.com>
2023-04-27 16:30:55 -0500

    ADDED MORE STUFF

--- library.py
+++ library.py
@@ -7,12 +7,26 @@ conn.close()
 root = Tk()
 
 root.title('USER_OPTIONS')
-root.geometry("600x600")
+root.geometry("550x500")
 
 
 def option_one():
-	print("this really sucks")
+	print("one")
 
+def option_two():
+	print("two")
+
+def option_three():
+	print("three")
+
+def option_four():
+	print("four")
+
+def option_five():
+	print("five")
+
+def option_six():
+	print("six")
 
 
 #Tell users what they can do
@@ -28,10 +42,91 @@ first_option = Entry(root, width = 30)
 first_option.grid(row = 2, column = 1)
 first_option.insert(0,"Check out a book")
 
+space = Label(root, text ="")
+space.grid(row = 3, column =1)
+
+second_option = Entry(root, width = 30)
+second_option.grid(row = 4, column = 1)
+second_option.insert(0,"Add a new borrower")
+
+space = Label(root, text ="")
+space.grid(row = 5, column =1)
+
+third_option = Entry(root, width = 30)
+third_option.grid(row = 6, column = 1)
+third_option.insert(0,"Add a book and publisher")
+
+space = Label(root, text ="")
+space.grid(row = 7, column =1)
+
+fourth_option = Entry(root, width = 30)
+fourth_option.grid(row = 8, column = 1)
+fourth_option.insert(0,"List book copies per branch")
+
+space = Label(root, text ="")
+space.grid(row = 9, column =1)
+
+fifth_option = Entry(root, width = 30)
+fifth_option.grid(row = 10, column = 1)
+fifth_option.insert(0,"Book return information")
+
+space = Label(root, text ="")
+space.grid(row = 11, column =1)
+
+sixth_option = Entry(root, width = 30)
+sixth_option.grid(row = 12, column = 1)
+sixth_option.insert(0,"Get a view result")
+
+#spaces before we add out names 
+space = Label(root, text ="")
+space.grid(row = 13, column =1)
+
+space = Label(root, text ="")
+space.grid(row = 14, column =1)
+
+name1 = Label(root, text = 'Joshua Lian')
+name1.grid(row = 15, column =1)
+
+space = Label(root, text ="")
+space.grid(row = 16, column =1)
+
+name2 = Label(root, text = 'Kierra Ashford')
+name2.grid(row = 17, column =1)
+
+space = Label(root, text ="")
+space.grid(row = 18, column =1)
+
+name3 = Label(root, text = 'Abhinav Shrestha')
+name3.grid(row = 19, column =1)
+
+
 #lables before the entry explanation
 opt1 = Label(root, text = 'OPTION 1')
 opt1.grid(row = 2, column =0)
 
+opt2 = Label(root, text = 'OPTION 2')
+opt2.grid(row = 4, column =0)
+
+opt3 = Label(root, text = 'OPTION 3')
+opt3.grid(row = 6, column =0)
+
+opt4 = Label(root, text = 'OPTION 4')
+opt4.grid(row = 8, column =0)
+
+opt5 = Label(root, text = 'OPTION 5')
+opt5.grid(row = 10, column =0)
+
+opt6 = Label(root, text = 'OPTION 6')
+opt6.grid(row = 12, column =0)
+
+
+
 #create buttons
 Button(root, text = "select", command = option_one).grid(row = 2, column =2)
+Button(root, text = "select", command = option_two).grid(row = 4, column =2)
+Button(root, text = "select", command = option_three).grid(row = 6, column =2)
+Button(root, text = "select", command = option_four).grid(row = 8, column =2)
+Button(root, text = "select", command = option_five).grid(row = 10, column =2)
+Button(root, text = "select", command = option_six).grid(row = 12, column =2)
+
 root.mainloop()
