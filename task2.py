3fa813d67708ede16b2137ec89e1041286bf4f11
Kierra Ashford <86200340+natasyakaramazov@users.noreply.github.com>
2023-04-28 15:00:13 -0500

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,67 @@
+from tkinter import *
+import sqlite3
+
+
+def option_two():
+	option2 = Tk()
+	option2.title("Insert Borrower")
+	option2.geometry("400x400")
+
+	option_conn = sqlite3.connect('library.db')
+	option_two_cur = option_conn.cursor()
+
+
+
+	
+	card_no = Entry(option2, width = 30)
+	card_no.grid(row = 1, column = 1)
+	
+	card_no_label = Label(option2, text ='Card_no') 
+	card_no_label.grid(row = 1, column = 0)
+	space = Label(option2, text ="")
+	space.grid(row = 2, column =0)
+
+
+	name = Entry(option2, width = 30)
+	name.grid(row = 3, column = 1)
+	
+	name_label = Label(option2, text = 'Name') 
+	name_label.grid(row = 3, column = 0)
+	space = Label(option2, text ="")
+	space.grid(row = 4, column =0)
+
+
+	phone= Entry(option2, width = 30)
+	phone.grid(row = 5, column = 1)
+	
+	phone_label = Label(option2, text = 'Phone') 
+	phone_label.grid(row = 5, column = 0)
+	space = Label(option2, text ="")
+	space.grid(row = 6, column =0)
+
+	
+	address = Entry(option2, width = 30)
+	address.grid(row = 7, column = 1)
+	
+	address_label = Label(option2, text = 'Address') 
+	address_label.grid(row = 7, column = 0)
+	space = Label(option2, text ="")
+	space.grid(row = 8, column =0)
+
+
+	submit_btn = Button(option2, text ='Add Borrower ', command = option_two_cur.execute)
+	submit_btn.grid(row = 10, column =0, columnspan = 2, pady = 10, padx = 10, ipadx = 140)
+
+
+	option_two_cur.execute("INSERT INTO BORROWERS VALUES (:card_no, :name, :phone, :address) ",
+		{   'card_no': card_no.get(),
+			'name': name.get(),
+			'phone': phone.get(),
+			'address': address.get(),
+			
+		})
+	option_conn.commit()
+	#close the DB connection
+	option_conn.close()
+	
+option_two()
